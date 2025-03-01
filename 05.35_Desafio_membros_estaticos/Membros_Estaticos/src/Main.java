8c69335e6656dd3d859005e50eab894063fd7797
ivan.ferreira <iaaninsane@email.com>
2025-03-01 09:33:12 -0300

    desafio membros estaticos

---
+++ 05.35_Desafio_membros_estaticos/Membros_Estaticos/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        double areaQuadrado = CalculadoraArea.calcularAreaQuadrado(5.2);
+        double areaCirculo = CalculadoraArea.calcularAreaCirculo(10.5);
+
+        System.out.printf("Área do quadrado: %.2f%n", areaQuadrado);
+        System.out.printf("Área do círculo: %.2f%n", areaCirculo);
+    }
+}
